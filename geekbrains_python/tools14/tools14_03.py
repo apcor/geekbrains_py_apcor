476e0f6ebf1421e6058c6d1509d5bc2ff2efae9e
Arsen Sogoyan <apcor@yandex.ru>
2020-01-26 15:55:20 +0300

    Changed **2 to math.sqrt() in task 3

--- geekbrains_python/tools14/tools14_03.py
+++ geekbrains_python/tools14/tools14_03.py
@@ -1,11 +1,11 @@
-import random
+import math, random
 
 num_list =[random.randint(-100,100) for i in range(1,20)]
 print(num_list)
 
 def modify_list(list):
     list = list[:]
-    return [element if element<0 else element**2 for element in list]
+    return [element if element<0 else math.sqrt(element) for element in list]
 
 print(modify_list(num_list))
 print(num_list)
