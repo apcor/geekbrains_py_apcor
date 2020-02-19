a150e053e5f55c587b4c58e19ea2ab862f68f816
Arsen Sogoyan <apcor@yandex.ru>
2020-02-20 02:27:05 +0300

    ДЗ 5 сделано

--- pybasics_webinars/Sogoyan_Arsen_DZ_5/task_1.py
+++ pybasics_webinars/Sogoyan_Arsen_DZ_5/task_1.py
@@ -1,4 +1,4 @@
-with open('new_file.txt', 'w', encoding = 'utf-8') as my_file:
+with open('task_1_file.txt', 'w', encoding = 'utf-8') as my_file:
     while True:
         user_str = input('Введите строку: ')
         my_file.writelines(f'{user_str} \n')
